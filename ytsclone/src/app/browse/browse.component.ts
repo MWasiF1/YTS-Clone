ea19dff6826a5a48f9078baae73cab38dfe5682f
MWasiF1 <mianwasif.001@gmai.com>
2024-08-09 18:11:19 +0500

    new changes

--- ytsclone/src/app/browse/browse.component.ts
+++ ytsclone/src/app/browse/browse.component.ts
@@ -7,22 +7,59 @@ import { MovieService } from '../movie.service';
   styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent implements OnInit {
-  
-  movies: any[] = [];
 
-  // Injecting the MovieService to fetch movie data
+  movies: any[] = [];
+  filteredMovies: any[] = [];
+  searchQuery: string = '';
+  selectedQuality: string = '';
+  selectedGenre: string = '';
+  selectedRating: string = '';
+  selectedYear: string = '';
+  selectedLanguage: string = '';
+  selectedOrderBy: string = '';
+
   constructor(private movieService: MovieService) { }
 
-  // Lifecycle hook runs after component's initialization
   ngOnInit(): void {
-    this.fetchMovies(); 
+    this.fetchMovies();
   }
-
 
   fetchMovies(): void {
     this.movieService.getMovies().subscribe(data => {
-      // Assign the fetched movie data to the movies array
-      this.movies = data.data.movies;
+      if (data && data.data && data.data.movies) {
+        this.movies = data.data.movies;
+        this.filteredMovies = this.movies;
+      } else {
+        console.error('Unexpected API response format:', data);
+      }
     });
+  }
+
+  applyFilters(): void {
+    this.filteredMovies = this.movies.filter(movie => {
+      const matchesQuery = this.searchQuery ? movie.title.toLowerCase().includes(this.searchQuery.toLowerCase()) : true;
+      const matchesQuality = this.selectedQuality ? movie.quality === this.selectedQuality : true;
+      const matchesGenre = this.selectedGenre ? movie.genres.includes(this.selectedGenre) : true;
+      const matchesRating = this.selectedRating ? movie.rating >= +this.selectedRating : true;
+      const matchesYear = this.selectedYear ? movie.year === +this.selectedYear : true;
+      const matchesLanguage = this.selectedLanguage ? movie.language === this.selectedLanguage : true;
+
+      return matchesQuery && matchesQuality && matchesGenre && matchesRating && matchesYear && matchesLanguage;
+    });
+
+    if (this.selectedOrderBy) {
+      this.filteredMovies.sort((a, b) => {
+        if (this.selectedOrderBy === 'year') {
+          return b.year - a.year;
+        } else if (this.selectedOrderBy === 'rating') {
+          return b.rating - a.rating;
+        }
+        return 0;
+      });
+    }
+  }
+
+  onFilterChange(): void {
+    this.applyFilters();
   }
 }
--- ytsclone/src/app/browse/browse.module.ts
+++ ytsclone/src/app/browse/browse.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BrowseComponent } from './browse.component';
 import { RouterModule } from '@angular/router'; 
 import { SharedModule } from '../shared/shared.module';  
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule, // Import to use common Angular directives and pipes
     RouterModule.forChild([{path:'',component: BrowseComponent}]), // Set up routing for this module
     SharedModule,  
+    FormsModule
   ],
   exports: [BrowseComponent]
 })
